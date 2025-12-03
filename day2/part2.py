8b4989357143c2a8ee7960c79d8c0c8109d1783b
SouravKumarBarman <souravkumarbarman1729@gmail.com>
2025-12-03 13:39:27 +0530

    day 2 done

---
+++ day2/part2.py
@@ -0,0 +1,24 @@
+import re
+
+def find_id_in_range(start:int, end:int):
+    sum=int(0)
+    for Id in range(start, end+1):
+        result=re.match(r'^([0-9]+)\1+$',str(Id))
+        if result:
+            print(result.string)
+            sum+=int(result.string)
+    return sum
+
+if __name__=='__main__':
+    file=open("input.txt",'r')
+    input = file.readline()
+    list_input=input.split(',')
+    array_of_array=[]
+    output=int(0)
+    for element in list_input:
+        temp=element.split('-')
+        array_of_array.append(temp)
+    for array in array_of_array:
+        output+=find_id_in_range(int(array[0]),int(array[1]))
+    print(output)
+        
